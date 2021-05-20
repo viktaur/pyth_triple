ec9afadada091e1f25e206b4e8815236ecc52020
viktaur <30535579+viktaur@users.noreply.github.com>
2021-05-20 14:58:25 +0200

    update and finish!

---
+++ main.py
@@ -0,0 +1,11 @@
+i = 1
+
+n = input("Select the desired number of triples: ")
+
+for i in range(0, eval(n)):
+    a = i ** 2 - 1
+    b = 2 * i
+    c = i ** 2 + 1
+    print(a, b, c)
+    i += 1
+30
