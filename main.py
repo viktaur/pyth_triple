ffc699a726004c1687f27e28b91fb7d0a2da3af8
viktaur <30535579+viktaur@users.noreply.github.com>
2021-05-24 16:27:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-# i = 0
+def several():
+    n = input("Introduce the desired number of triples: ")
 
-n = input("Introduce the desired number of triples: ")
+    for i in range(1, eval(n) + 1):
+        a = i ** 2 - 1
+        b = 2 * i
+        c = i ** 2 + 1
+        print(a, b, c)
 
-for i in range(0, eval(n)):
-    a = i ** 2 - 1
-    b = 2 * i
-    c = i ** 2 + 1
+def one():
+    n = eval(input("Introduce the triple index: "))
+
+    a = n ** 2 - 1
+    b = 2 * n
+    c = n ** 2 + 1
     print(a, b, c)
-    i += 1
+
+print("[1] Display a desired number of triples (from index 0)")
+print("[2] Display just one triple by knowing its index (instantaneous)")
+q = input("Please select one of above: ")
+
+if q == "1":
+    several()
+elif q == "2":
+    one()
+else:
+    print("ERROR: exiting...")
+    exit()
